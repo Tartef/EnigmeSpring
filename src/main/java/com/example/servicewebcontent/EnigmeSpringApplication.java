010d849f3fae28dcf5dec4822b22df81ed3d54a1
FlorianC <florian8702@gmail.com>
2022-12-07 11:08:00 +0100

    Ajout du fichier index.html et affichage de ce dernier

---
+++ src/main/java/com/example/servicewebcontent/EnigmeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicewebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servicewebcontent/Test.java
@@ -0,0 +1,2 @@
+package com.example.servicewebcontent;public class Test {
+}
