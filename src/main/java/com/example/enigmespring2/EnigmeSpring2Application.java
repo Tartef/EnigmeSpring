6f00d95e1fa152c578423735b43908ed69981541
FlorianC <florian8702@gmail.com>
2022-12-12 10:52:50 +0100

    First commit

---
+++ src/main/java/com/example/enigmespring2/EnigmeSpring2Application.java
@@ -0,0 +1,13 @@
+package com.example.enigmespring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmeSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnigmeSpring2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enigmespring2/EnigmeSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enigmespring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmeSpring2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
